0110c34a539f065d673d8482c0bdb0f0453f7358
Ali <samolet.74@mail.ru>
2023-02-23 14:52:56 +0300

    problems.

---
+++ hw-p.js
@@ -0,0 +1,75 @@
+
+// задача
+ let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+
+// задача
+let iphone = '9 января 2007 год';
+alert(iphone);
+
+
+// задача
+let javaScript = 'Брендан Эйх';
+alert (javaScript);
+
+
+// задача
+let b = 2;
+let c = 10;
+
+let d = b+c;
+let e = c-b;
+let f = b*c;
+let g = c/b;
+
+alert(d);
+alert(e);
+alert(f);
+alert(g);
+
+
+// задача
+let result = 2**5;
+alert(result);
+
+
+// задача
+let percent = 9%2;
+alert(percent);
+
+
+// задача
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+
+// задача
+let age = prompt('Сколько вам лет?');
+alert(age);
+
+
+// задача
+const user = {
+  name:String('Frodo'),
+  age:Number(36),
+  isAdmin:Boolean(false)
+};
+user["city of residence"] = 'Shir';
+user.age=4400;
+delete user["city of residence"];
+let info = prompt('Какую информацию хотите узнать о пользователе?');
+alert(user[info]);
+
+
+// задача
+let task = prompt('как Вас зовут?');
+alert(task);
