46835c5e18c32b8619f109c9d9e66bd8eb94dffb
Ali <samolet.74@mail.ru>
2023-03-06 23:11:45 +0300

    css.

---
+++ function comparison(a, b) {.js
@@ -0,0 +1,20 @@
+function comparison(a, b) {
+    if (a < b){
+      return a;
+    } else {
+      return b;
+    }
+  };
+
+
+  function returnLine(a) {
+    if (a % 2 == 0){
+      return `число четное`;
+    } else {
+      return `число нечетное`;
+    }
+  };
+  
+  returnLine(`33`);
+
+  
